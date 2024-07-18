2f81ac7fdcefb6efbde05970440602d778ac494b
Marta Prystatska <tradlisboa@gmail.com>
2024-07-18 21:38:29 +0100

    finished  TO-DO LIST +complete scss

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,28 +1,33 @@
 import PropTypes from "prop-types";
+import cx from "classnames";
+import styles from './Task.module.scss';
 
 const Task = (props) => {
   const {
-      task: { id, content, isDone },
-      setIsDone,
-      deleteTask,
+    task: { id, content, isDone },
+    setIsDone,
+    deleteTask,
   } = props;
+
+  const spanClasses = cx(styles.content, { [styles['is-done']]: isDone });
+
   return (
-    <li>
-          <input type="checkbox" checked={isDone} onChange={()=>setIsDone(id)} />
-      <span >{content}</span>
-          <button onClick={() => {deleteTask(id)}}>X</button>
+    <li className={styles.taskItem}>
+      <input type="checkbox" checked={isDone} onChange={() => setIsDone(id)} />
+      <span className={spanClasses}>{content}</span>
+      <button onClick={() => deleteTask(id)}>X</button>
     </li>
   );
 };
 
 Task.propTypes = {
-    task: PropTypes.shape({                            //shape significa [objet]
-    id: PropTypes.string.isRequired,              
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     isDone: PropTypes.bool.isRequired,
   }),
-    setIsDone: PropTypes.func,
-    deleteTask: PropTypes.func,
+  setIsDone: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,36 +1,47 @@
 import { Formik, Form, Field } from "formik";
 import useTodo from "../../hooks/useTodo";
 import Task from "../Task";
-import styles from "./Todo.module.scss"; 
+import styles from "./Todo.module.scss";
 
 const Todo = () => {
   const { tasks, addTask, setIsDone, deleteTask } = useTodo([
-    { id: '1', content: "test task", isDone: false },
+    { id: "1", content: "test task", isDone: false },
   ]);
+
   const onSubmit = (values, formikBag) => {
     addTask(values);
     formikBag.resetForm();
   };
 
-
   return (
-    <section className={styles.todoContainer}> 
-      <h2>Todo</h2>
-      <div>
-        <Formik initialValues={{ content:''}} onSubmit={onSubmit}>
-          <Form>
-            <Field name="content" placeholder="Enter task" />
-            <input type="submit" value="Add" />
+    <section className={styles.todoContainer}>
+      <h2 className={styles.header}>To-Do List</h2>
+
+      <div className={styles.taskListContainer}>
+        <h3 className={styles.title}>Tasks</h3>
+        <ul className={styles.taskList}>
+          {tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              setIsDone={setIsDone}
+              deleteTask={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className={styles.formContainer}>
+        <h3 className={styles.title}>New Task</h3>
+        <Formik initialValues={{ content: "" }} onSubmit={onSubmit}>
+          <Form className={styles.form}>
+            <Field
+              name="content"
+              //placeholder="Enter task"
+              className={styles.input}
+            />
+            <input type="submit" value="ADD" className={styles.addButton} />
           </Form>
         </Formik>
-      </div>
-      <div>
-        <h3>List Tasks</h3>
-        <ol>
-          {tasks.map((task) => (
-            <Task key={task.id} task={task} setIsDone={setIsDone} deleteTask={deleteTask} />
-          ))}
-        </ol>
       </div>
     </section>
   );
