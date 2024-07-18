986747b62ead7ea2e2bcd6f2f5d98213233e2900
Marta Prystatska <tradlisboa@gmail.com>
2024-07-18 13:24:54 +0100

    create hook folder & useTodo;

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,9 +1,25 @@
-
+import useTodo from "../../hooks/useTodo";
 
 const Todo = () => {
+  const [tasks, setTasks] = useTodo([
+    { id: 1, content: "content", isDone: false },
+  ]);
+
     return (
-        <div>todo</div>
-    );
-}
+      
+        
+    <section>
+      <h2>Todo</h2>
+      <div>
+        <h3>List Tasks</h3>
+        <ol>
+          {tasks.map((task) => (
+            <li key={task.id}>{task.content}</li>
+          ))}
+        </ol>
+      </div>
+    </section>
+  );
+};
 
 export default Todo;
