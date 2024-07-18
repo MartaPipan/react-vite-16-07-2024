20b2b9dfdcf6860ab5faa0c1ddadd0dc58b9164b
Marta Prystatska <tradlisboa@gmail.com>
2024-07-18 17:18:57 +0100

    add task + create Todo.module.scss; + INSTALL  uuid -NPM;

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,30 +1,32 @@
-import { Formik, Form, Field  } from "formik";
+import { Formik, Form, Field } from "formik";
 import useTodo from "../../hooks/useTodo";
+import styles from "./Todo.module.scss"; 
 
 const Todo = () => {
-    const { tasks, addTask } = useTodo([
-    { id: 1, content: "content", isDone: false },
+  const { tasks, addTask } = useTodo([
+    { id: 1, content: "test task", isDone: false },
   ]);
 
-    return  <section>
-            <h2>Todo</h2>
-            <div>
-                <Formik initialValues={{content:{}}} onSubmit={addTask}>
-                    <Form>
-                        <Field name='content'/>
-                            <input type="submit" value="Add" />
-                    </Form>
-                </Formik>
-            </div>
+  return (
+    <section className={styles.todoContainer}> 
+      <h2>Todo</h2>
+      <div>
+        <Formik initialValues={{ content:''}} onSubmit={addTask}>
+          <Form>
+            <Field name="content" placeholder="Enter task" />
+            <input type="submit" value="Add" />
+          </Form>
+        </Formik>
+      </div>
       <div>
         <h3>List Tasks</h3>
         <ol>
-          {tasks.map((task) => (
-            <li key={task.id}>{task.content}</li>
-          ))}
+          {tasks.map((task) => <li key={task.id}>{task.content}</li>
+          )}
         </ol>
       </div>
     </section>
+  );
 };
 
 export default Todo;
--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 
 const useTodo = (initialTasks) => {
-    const [tasks, setTasks] = useState([...initialTasks]);
+  const [tasks, setTasks] = useState([...initialTasks]);
 
-    return { tasks, setTasks };
+  const addTask = (values) => {
+    const newTask = {
+      id: uuidv4(),
+      content: values.content,
+      isDone: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  return { tasks, setTasks, addTask };
 };
+
 export default useTodo;
