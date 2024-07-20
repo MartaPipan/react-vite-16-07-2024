017c728542b08a871e7cdf79029631ac85384de1
Marta Prystatska <tradlisboa@gmail.com>
2024-07-20 21:57:46 +0100

    UPDATE  useTODO for TO-DO -LIST

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,31 +1,31 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-const useTodo = (initialTasks) => {
-  const [tasks, setTasks] = useState([...initialTasks]);
-  
-  const addTask = (values) => {
+const useTodo = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (content) => {
     const newTask = {
       id: uuidv4(),
-      content: values.content,
+      content,
       isDone: false,
     };
     setTasks(prevTasks => [...prevTasks, newTask]);
   };
 
-   const setIsDone = (id) => { 
-        const newTasks = tasks.map((task) =>
-            task.id === id ? { ...task, isDone: true } : task);
-        setTasks(newTasks);
-   }
-  
-    const deleteTask = (id) => {
+  const setIsDone = (id) => {
+    setTasks(prevTasks =>
+      prevTasks.map(task =>
+        task.id === id ? { ...task, isDone: !task.isDone } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
     setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
   };
 
-
-
-  return { tasks, addTask, setIsDone, deleteTask};
+  return { tasks, addTask, setIsDone, deleteTask };
 };
 
 export default useTodo;
